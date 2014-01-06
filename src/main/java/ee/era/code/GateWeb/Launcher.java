a37f331eb2bb1868952f0a6890bd974f688ac2e9
Valeri <orhdposit@gmail.com>
2014-01-06 13:47:53 +0200

    Web server started

--- src/main/java/ee/era/code/GateWeb/Launcher.java
+++ src/main/java/ee/era/code/GateWeb/Launcher.java
@@ -1,12 +1,41 @@
 package ee.era.code.GateWeb;
 
 import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Launcher {
     private static final Logger LOG = Logger.getLogger(Launcher.class);
-    public static void main(String args[])
-    {
+    public static final long startTimestamp = System.currentTimeMillis();
+
+    public static void main(String args[]) throws Exception {
         LOG.error("Starting Gate");
-        System.out.println("Started");
+        Launcher launcher = new Launcher();
+        launcher.run();
+    }
+
+    void run() throws Exception {
+        Server server = new Server();
+        ServerConnector http = getHttpConnector(server);
+        server.setConnectors(new Connector[]{http});
+        server.setHandler(createHandler());
+        server.start();
+        LOG.info("Gate started in " + (System.currentTimeMillis() - startTimestamp) + " ms");
+        server.join();
+    }
+
+    private Handler createHandler() {
+        WebAppContext webAppContext = new WebAppContext("webapp", "/");
+        //webAppContext.setErrorHandler(new PlainTextErrorHandler());
+        webAppContext.setMaxFormContentSize(20 * 1024 * 1024);
+        webAppContext.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "true");
+        return webAppContext;
+    }
+
+    private ServerConnector getHttpConnector(Server server) {
+        ServerConnector http= new ServerConnector(server, new HttpConnectionFactory(new HttpConfiguration()));
+        http.setPort(8080);
+        http.setIdleTimeout(30000);
+        return http;
     }
 }
